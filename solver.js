7196f84a5e466eaeafa28fd00fdab73e584fa85e
Yuvakishore-M-git <87435912+Yuvakishore-M-git@users.noreply.github.com>
2022-10-11 23:31:08 +0530

    updated solver.js
    when solve() is called, raw and ans are updated

--- solver.js
+++ solver.js
@@ -101,9 +101,9 @@ function solve(){
         sudoku.push(x)
     }
     raw=new Array()
-    for(let i=0;i<9;i++){
+    for (let i = 0; i < 9; i++) {
         let x=new Array()
-        for(let j=0;j<9;j++){
+        for (let j = 0; j < 9; j++) {
             x.push(sudoku[i][j])
         }
         raw.push(x)
